75d055cb2931cc983fbd9e854f794d164b5c41d1
jabyl <jabyl.new@gmail.com>
2026-03-19 10:51:17 +0100

    completed Mixpanel's integration, redeploying with enhanced debugging + tracking

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import App from "./App";
 import "./index.css";
 import posthog from "posthog-js";
 import { PostHogProvider } from "@posthog/react";
+import { initMixpanel } from "./lib/mixpanel";
+
+initMixpanel();
 
 posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_TOKEN, {
   api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
